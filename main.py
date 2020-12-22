4a0e8e66543458a025da0af50c7dad1cd289bd99
Maciej Kusz <maciej.kusz@ac-project.net>
2020-12-22 11:56:21 +0100

    Smallest possible code

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 import unittest, xmlrunner, json, requests
 
-class Tests(unittest.TestCase): pass
-
-def add_test(cls, name, data):
-    def abstract_test(self):
-        self.assertEqual(requests.request(**data['request']).status_code, data['assert']['statusCode'])
-    setattr(cls, name, abstract_test)
-
 with open('tests.json', 'r') as json_file:
-    for test_name, test_data in json.load(json_file).items():
-        add_test(Tests, test_name, test_data)
+    Tests = type("Tests", (unittest.TestCase,), {name: (lambda data: lambda self: self.assertEqual(requests.request(**data['request']).status_code, data['assert']['statusCode']))(data) for name, data in json.load(json_file).items()})
 
-if __name__ == '__main__':
-    unittest.main(testRunner=xmlrunner.XMLTestRunner())
+unittest.main(testRunner=xmlrunner.XMLTestRunner())

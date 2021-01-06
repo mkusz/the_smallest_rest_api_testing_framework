c3d3b633a9cad90a975ccaad7c1ea07960615650
Maciej Kusz <maciej.kusz@ac-project.net>
2021-01-06 20:30:47 +0100

    PEP-8 valid

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import unittest, xmlrunner, json, requests, glob
-
-def abstract_test(self, data):
-    response: requests.Response = requests.request(**data['request'])
-    self.assertEqual(response.status_code, data['assert']['statusCode'])
-    self.assertSetEqual(set(response.json().keys()), set(data['assert']['responseKeys']))
-    self.assertLessEqual(response.elapsed.total_seconds(), data['assert']['responseTime'])
+import unittest
+import xmlrunner
+import json
+import requests
+import glob
 
 for file_name in glob.iglob("*.json"):
     with open(file_name, 'r') as json_file:
-        test = lambda data: lambda self: abstract_test(self, data)
+        tests_dict = {name: (lambda data: lambda self: self.assertEqual(
+            requests.request(**data['request']).status_code, data['assert']['statusCode']))(data)
+                      for name, data in json.load(json_file).items()}
         suite_name = file_name.split('.')[0]
-        globals()[suite_name] = type(suite_name, (unittest.TestCase,), {name: test(data) for name, data in json.load(json_file).items()})
+        globals()[suite_name] = type(suite_name, (unittest.TestCase,), tests_dict)
 
 unittest.main(testRunner=xmlrunner.XMLTestRunner())
